05fba288ac1b2c05f71336c105f84947519fee66
zakir abbas <zakirabbas023@gmail.com>
2024-05-02 17:23:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Items from './components/Items';
+import Navbar from './components/Navbar';
+import CustomItemContext from './itemContext';
+function App() {
+
+  return (
+    <>
+      <CustomItemContext>
+        {/* ye sab children isko props ke zariyen access kar sakte hain */}
+        <div className='App'>
+          <h2>Shopping Cart</h2>
+          <Navbar />
+          <Items />
+        </div>
+      </CustomItemContext>
+
+
+    </>
+  );
+}
+export default App;
